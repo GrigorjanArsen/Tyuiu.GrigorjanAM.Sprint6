959fcfd529863ae3455f631d50830c30f1d47316
Tyuiu.GrigorjanAM@outlook.com <djuravlik323@gmail.com>
2023-12-09 12:02:58 +0500

    fix

--- Tyuiu.GrigorjanAM.Sprint6.Task6.V29/FormMain.Designer.cs
+++ Tyuiu.GrigorjanAM.Sprint6.Task6.V29/FormMain.Designer.cs
@@ -32,25 +32,25 @@ namespace Tyuiu.GrigorjanAM.Sprint6.Task6.V29
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
             this.panelButtons_GAM = new System.Windows.Forms.Panel();
-            this.panelCondition_GAM = new System.Windows.Forms.Panel();
-            this.panelInPut_GAM = new System.Windows.Forms.Panel();
-            this.buttonOpenFIle_GAM = new System.Windows.Forms.Button();
-            this.buttonDone_GAM = new System.Windows.Forms.Button();
             this.buttonHelp_GAM = new System.Windows.Forms.Button();
+            this.buttonDone_GAM = new System.Windows.Forms.Button();
+            this.buttonOpenFIle_GAM = new System.Windows.Forms.Button();
+            this.panelCondition_GAM = new System.Windows.Forms.Panel();
             this.groupBoxCondition_GAM = new System.Windows.Forms.GroupBox();
-            this.groupBoxInPut_GAM = new System.Windows.Forms.GroupBox();
             this.textBoxCondition_GAM = new System.Windows.Forms.TextBox();
+            this.panelInPut_GAM = new System.Windows.Forms.Panel();
+            this.groupBoxInPut_GAM = new System.Windows.Forms.GroupBox();
             this.textBoxInPut_GAM = new System.Windows.Forms.TextBox();
             this.openFileDialogOpen_GAM = new System.Windows.Forms.OpenFileDialog();
             this.toolTip_GAM = new System.Windows.Forms.ToolTip(this.components);
             this.panelOutPut_GAM = new System.Windows.Forms.Panel();
             this.groupBoxOutPut_GAM = new System.Windows.Forms.GroupBox();
-            this.textBoxOutPut_GAM = new System.Windows.Forms.TextBox();
             this.splitter2 = new System.Windows.Forms.Splitter();
+            this.textBoxOutPut_GAM = new System.Windows.Forms.TextBox();
             this.panelButtons_GAM.SuspendLayout();
             this.panelCondition_GAM.SuspendLayout();
-            this.panelInPut_GAM.SuspendLayout();
             this.groupBoxCondition_GAM.SuspendLayout();
+            this.panelInPut_GAM.SuspendLayout();
             this.groupBoxInPut_GAM.SuspendLayout();
             this.panelOutPut_GAM.SuspendLayout();
             this.groupBoxOutPut_GAM.SuspendLayout();
@@ -67,48 +67,6 @@ namespace Tyuiu.GrigorjanAM.Sprint6.Task6.V29
             this.panelButtons_GAM.Size = new System.Drawing.Size(704, 65);
             this.panelButtons_GAM.TabIndex = 0;
             // 
-            // panelCondition_GAM
-            // 
-            this.panelCondition_GAM.Controls.Add(this.groupBoxCondition_GAM);
-            this.panelCondition_GAM.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelCondition_GAM.Location = new System.Drawing.Point(0, 65);
-            this.panelCondition_GAM.Name = "panelCondition_GAM";
-            this.panelCondition_GAM.Size = new System.Drawing.Size(704, 70);
-            this.panelCondition_GAM.TabIndex = 0;
-            // 
-            // panelInPut_GAM
-            // 
-            this.panelInPut_GAM.Controls.Add(this.groupBoxInPut_GAM);
-            this.panelInPut_GAM.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panelInPut_GAM.Location = new System.Drawing.Point(0, 135);
-            this.panelInPut_GAM.Name = "panelInPut_GAM";
-            this.panelInPut_GAM.Size = new System.Drawing.Size(360, 306);
-            this.panelInPut_GAM.TabIndex = 0;
-            // 
-            // buttonOpenFIle_GAM
-            // 
-            this.buttonOpenFIle_GAM.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonOpenFIle_GAM.BackgroundImage")));
-            this.buttonOpenFIle_GAM.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.buttonOpenFIle_GAM.ImageKey = "(none)";
-            this.buttonOpenFIle_GAM.Location = new System.Drawing.Point(12, 3);
-            this.buttonOpenFIle_GAM.Name = "buttonOpenFIle_GAM";
-            this.buttonOpenFIle_GAM.Size = new System.Drawing.Size(70, 58);
-            this.buttonOpenFIle_GAM.TabIndex = 0;
-            this.buttonOpenFIle_GAM.UseVisualStyleBackColor = true;
-            this.buttonOpenFIle_GAM.Click += new System.EventHandler(this.buttonOpenFIle_GAM_Click);
-            // 
-            // buttonDone_GAM
-            // 
-            this.buttonDone_GAM.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonDone_GAM.BackgroundImage")));
-            this.buttonDone_GAM.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.buttonDone_GAM.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.buttonDone_GAM.Location = new System.Drawing.Point(88, 3);
-            this.buttonDone_GAM.Name = "buttonDone_GAM";
-            this.buttonDone_GAM.Size = new System.Drawing.Size(70, 58);
-            this.buttonDone_GAM.TabIndex = 0;
-            this.buttonDone_GAM.UseVisualStyleBackColor = true;
-            this.buttonDone_GAM.Click += new System.EventHandler(this.buttonDone_GAM_Click);
-            // 
             // buttonHelp_GAM
             // 
             this.buttonHelp_GAM.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
@@ -119,8 +77,46 @@ namespace Tyuiu.GrigorjanAM.Sprint6.Task6.V29
             this.buttonHelp_GAM.Name = "buttonHelp_GAM";
             this.buttonHelp_GAM.Size = new System.Drawing.Size(58, 58);
             this.buttonHelp_GAM.TabIndex = 0;
+            this.toolTip_GAM.SetToolTip(this.buttonHelp_GAM, "Сведения о программе");
             this.buttonHelp_GAM.UseVisualStyleBackColor = true;
             this.buttonHelp_GAM.Click += new System.EventHandler(this.buttonHelp_GAM_Click);
+            // 
+            // buttonDone_GAM
+            // 
+            this.buttonDone_GAM.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonDone_GAM.BackgroundImage")));
+            this.buttonDone_GAM.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.buttonDone_GAM.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonDone_GAM.Enabled = false;
+            this.buttonDone_GAM.Location = new System.Drawing.Point(88, 3);
+            this.buttonDone_GAM.Name = "buttonDone_GAM";
+            this.buttonDone_GAM.Size = new System.Drawing.Size(70, 58);
+            this.buttonDone_GAM.TabIndex = 0;
+            this.toolTip_GAM.SetToolTip(this.buttonDone_GAM, "Производит поиск в файле вхождений символа \"i\"\r\nи конкатенирует строки, в которых" +
+        " находится\r\nэтот символ");
+            this.buttonDone_GAM.UseVisualStyleBackColor = true;
+            this.buttonDone_GAM.Click += new System.EventHandler(this.buttonDone_GAM_Click);
+            // 
+            // buttonOpenFIle_GAM
+            // 
+            this.buttonOpenFIle_GAM.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonOpenFIle_GAM.BackgroundImage")));
+            this.buttonOpenFIle_GAM.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.buttonOpenFIle_GAM.ImageKey = "(none)";
+            this.buttonOpenFIle_GAM.Location = new System.Drawing.Point(12, 3);
+            this.buttonOpenFIle_GAM.Name = "buttonOpenFIle_GAM";
+            this.buttonOpenFIle_GAM.Size = new System.Drawing.Size(70, 58);
+            this.buttonOpenFIle_GAM.TabIndex = 0;
+            this.toolTip_GAM.SetToolTip(this.buttonOpenFIle_GAM, "Открыть файл\r\nВыберите нужный файл для обработки");
+            this.buttonOpenFIle_GAM.UseVisualStyleBackColor = true;
+            this.buttonOpenFIle_GAM.Click += new System.EventHandler(this.buttonOpenFIle_GAM_Click);
+            // 
+            // panelCondition_GAM
+            // 
+            this.panelCondition_GAM.Controls.Add(this.groupBoxCondition_GAM);
+            this.panelCondition_GAM.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelCondition_GAM.Location = new System.Drawing.Point(0, 65);
+            this.panelCondition_GAM.Name = "panelCondition_GAM";
+            this.panelCondition_GAM.Size = new System.Drawing.Size(704, 64);
+            this.panelCondition_GAM.TabIndex = 0;
             // 
             // groupBoxCondition_GAM
             // 
@@ -128,21 +124,10 @@ namespace Tyuiu.GrigorjanAM.Sprint6.Task6.V29
             this.groupBoxCondition_GAM.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBoxCondition_GAM.Location = new System.Drawing.Point(0, 0);
             this.groupBoxCondition_GAM.Name = "groupBoxCondition_GAM";
-            this.groupBoxCondition_GAM.Size = new System.Drawing.Size(704, 70);
+            this.groupBoxCondition_GAM.Size = new System.Drawing.Size(704, 64);
             this.groupBoxCondition_GAM.TabIndex = 0;
             this.groupBoxCondition_GAM.TabStop = false;
             this.groupBoxCondition_GAM.Text = "Условие:";
-            // 
-            // groupBoxInPut_GAM
-            // 
-            this.groupBoxInPut_GAM.Controls.Add(this.textBoxInPut_GAM);
-            this.groupBoxInPut_GAM.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBoxInPut_GAM.Location = new System.Drawing.Point(0, 0);
-            this.groupBoxInPut_GAM.Name = "groupBoxInPut_GAM";
-            this.groupBoxInPut_GAM.Size = new System.Drawing.Size(360, 306);
-            this.groupBoxInPut_GAM.TabIndex = 0;
-            this.groupBoxInPut_GAM.TabStop = false;
-            this.groupBoxInPut_GAM.Text = "Ввод :";
             // 
             // textBoxCondition_GAM
             // 
@@ -153,9 +138,29 @@ namespace Tyuiu.GrigorjanAM.Sprint6.Task6.V29
             this.textBoxCondition_GAM.Multiline = true;
             this.textBoxCondition_GAM.Name = "textBoxCondition_GAM";
             this.textBoxCondition_GAM.ReadOnly = true;
-            this.textBoxCondition_GAM.Size = new System.Drawing.Size(698, 51);
+            this.textBoxCondition_GAM.Size = new System.Drawing.Size(698, 45);
             this.textBoxCondition_GAM.TabIndex = 0;
             this.textBoxCondition_GAM.Text = resources.GetString("textBoxCondition_GAM.Text");
+            // 
+            // panelInPut_GAM
+            // 
+            this.panelInPut_GAM.Controls.Add(this.groupBoxInPut_GAM);
+            this.panelInPut_GAM.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelInPut_GAM.Location = new System.Drawing.Point(0, 129);
+            this.panelInPut_GAM.Name = "panelInPut_GAM";
+            this.panelInPut_GAM.Size = new System.Drawing.Size(360, 312);
+            this.panelInPut_GAM.TabIndex = 0;
+            // 
+            // groupBoxInPut_GAM
+            // 
+            this.groupBoxInPut_GAM.Controls.Add(this.textBoxInPut_GAM);
+            this.groupBoxInPut_GAM.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxInPut_GAM.Location = new System.Drawing.Point(0, 0);
+            this.groupBoxInPut_GAM.Name = "groupBoxInPut_GAM";
+            this.groupBoxInPut_GAM.Size = new System.Drawing.Size(360, 312);
+            this.groupBoxInPut_GAM.TabIndex = 0;
+            this.groupBoxInPut_GAM.TabStop = false;
+            this.groupBoxInPut_GAM.Text = "Ввод :";
             // 
             // textBoxInPut_GAM
             // 
@@ -165,21 +170,26 @@ namespace Tyuiu.GrigorjanAM.Sprint6.Task6.V29
             this.textBoxInPut_GAM.Name = "textBoxInPut_GAM";
             this.textBoxInPut_GAM.ReadOnly = true;
             this.textBoxInPut_GAM.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBoxInPut_GAM.Size = new System.Drawing.Size(354, 287);
+            this.textBoxInPut_GAM.Size = new System.Drawing.Size(354, 293);
             this.textBoxInPut_GAM.TabIndex = 0;
             // 
             // openFileDialogOpen_GAM
             // 
             this.openFileDialogOpen_GAM.FileName = "openFileDialog1";
             // 
+            // toolTip_GAM
+            // 
+            this.toolTip_GAM.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.toolTip_GAM.ToolTipTitle = "Подсказка";
+            // 
             // panelOutPut_GAM
             // 
             this.panelOutPut_GAM.BackColor = System.Drawing.SystemColors.Control;
             this.panelOutPut_GAM.Controls.Add(this.groupBoxOutPut_GAM);
             this.panelOutPut_GAM.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelOutPut_GAM.Location = new System.Drawing.Point(360, 135);
+            this.panelOutPut_GAM.Location = new System.Drawing.Point(360, 129);
             this.panelOutPut_GAM.Name = "panelOutPut_GAM";
-            this.panelOutPut_GAM.Size = new System.Drawing.Size(344, 306);
+            this.panelOutPut_GAM.Size = new System.Drawing.Size(344, 312);
             this.panelOutPut_GAM.TabIndex = 0;
             // 
             // groupBoxOutPut_GAM
@@ -189,10 +199,18 @@ namespace Tyuiu.GrigorjanAM.Sprint6.Task6.V29
             this.groupBoxOutPut_GAM.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBoxOutPut_GAM.Location = new System.Drawing.Point(0, 0);
             this.groupBoxOutPut_GAM.Name = "groupBoxOutPut_GAM";
-            this.groupBoxOutPut_GAM.Size = new System.Drawing.Size(344, 306);
+            this.groupBoxOutPut_GAM.Size = new System.Drawing.Size(344, 312);
             this.groupBoxOutPut_GAM.TabIndex = 3;
             this.groupBoxOutPut_GAM.TabStop = false;
             this.groupBoxOutPut_GAM.Text = "Вывод:";
+            // 
+            // splitter2
+            // 
+            this.splitter2.Location = new System.Drawing.Point(3, 16);
+            this.splitter2.Name = "splitter2";
+            this.splitter2.Size = new System.Drawing.Size(3, 293);
+            this.splitter2.TabIndex = 1;
+            this.splitter2.TabStop = false;
             // 
             // textBoxOutPut_GAM
             // 
@@ -202,16 +220,8 @@ namespace Tyuiu.GrigorjanAM.Sprint6.Task6.V29
             this.textBoxOutPut_GAM.Name = "textBoxOutPut_GAM";
             this.textBoxOutPut_GAM.ReadOnly = true;
             this.textBoxOutPut_GAM.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBoxOutPut_GAM.Size = new System.Drawing.Size(338, 287);
+            this.textBoxOutPut_GAM.Size = new System.Drawing.Size(338, 293);
             this.textBoxOutPut_GAM.TabIndex = 0;
-            // 
-            // splitter2
-            // 
-            this.splitter2.Location = new System.Drawing.Point(3, 16);
-            this.splitter2.Name = "splitter2";
-            this.splitter2.Size = new System.Drawing.Size(3, 287);
-            this.splitter2.TabIndex = 1;
-            this.splitter2.TabStop = false;
             // 
             // FormMain
             // 
@@ -228,9 +238,9 @@ namespace Tyuiu.GrigorjanAM.Sprint6.Task6.V29
             this.Text = "Спринт 6 | Таск 6 | Вариант 29 | Григорян А.М.";
             this.panelButtons_GAM.ResumeLayout(false);
             this.panelCondition_GAM.ResumeLayout(false);
-            this.panelInPut_GAM.ResumeLayout(false);
             this.groupBoxCondition_GAM.ResumeLayout(false);
             this.groupBoxCondition_GAM.PerformLayout();
+            this.panelInPut_GAM.ResumeLayout(false);
             this.groupBoxInPut_GAM.ResumeLayout(false);
             this.groupBoxInPut_GAM.PerformLayout();
             this.panelOutPut_GAM.ResumeLayout(false);
